7bb9af4b4a9d11a8e0b8bd9e06bbdc7a6ea273fa
Christian Rupp <privat@christian-rupp.de>
2015-04-27 08:37:31 +0200

    Function stubs added!

--- python/ccookie.py
+++ python/ccookie.py
@@ -11,7 +11,26 @@ class ccookie:
 	def login(user, password):
 		pass
 
+	def addValue(self, keyword, value):
+		pass
+
+	def deleteValue(self, keyword):
+		pass
+
+	def getValue(self, keyword):
+		pass
+
+	def __encrypt(self, strin):
+		pass
+
+	def __decrypt(self, strin):
+		pass
+
+	def isValid(self,strin):
+		pass
+
 	def getKey(self):
+		self.isValid()
 		if os.path.isfile('key.asc'):
 			pass
 		else:
