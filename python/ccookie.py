49208050312f100f0fff7536c17206bfbab8cdbf
Christian Rupp <privat@christian-rupp.de>
2015-04-27 12:44:55 +0200

    Padding Function added!

--- python/ccookie.py
+++ python/ccookie.py
@@ -1,5 +1,6 @@
 import time
 from Crypto.Random import random
+from Crypto import Random
 import string
 import os
 from Crypto.Cipher import AES
@@ -8,7 +9,8 @@ import unicodedata
 class ccookie:
 
 	def __init__(self):
-		pass
+		self.getKey()
+		self.getInitialVector()
 
 	def login(user, password):
 		pass
@@ -23,10 +25,19 @@ class ccookie:
 		pass
 
 	def __encrypt(self, strin):
-		return AES.new(str.encode(self.__key), AES.MODE_ECB).encrypt(strin)
+		return AES.new(str.encode(self.__key), AES.MODE_ECB, self.__IV).encrypt(self.__pad(strin))
 
 	def __decrypt(self, strin):
-		return AES.new(str.encode(self.__key), AES.MODE_ECB).decrypt(strin)
+		return self.__unpad(AES.new(str.encode(self.__key), AES.MODE_ECB, self.__IV).decrypt(strin))
+
+	def __pad(self, strin):
+		i = 16 - (len(strin)%16)
+		for j in range(i):
+			strin += '\x0b'
+		return strin
+
+	def __unpad(self, strin):
+		pass
 
 	def isValid(self):
 		return 1
@@ -44,11 +55,21 @@ class ccookie:
 				f.close()
 			return self.__key
 
-	def getInitalVektor(self):
-		pass
+	def getInitialVector(self):
+		if self.isValid():
+			if os.path.isfile('initalVector.asc'):
+				f = open('initialVector.asc', 'r')
+				self.__IV = f.read()
+				f.close()
+			else:
+				f = open('initialVector.asc', 'w')
+				self.__IV = self.__generateInitialVector()
+				f.write(str(self.__IV))
+				f.close()
+			return self.__IV
 
-	def __generateInitialVektor(self):
-		pass
+	def __generateInitialVector(self):
+		return Random.new().read(AES.block_size)
 
 	def __generateKey(self):
 		return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(16))
